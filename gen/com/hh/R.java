4a05a29369e4b4281a75bdef42a6e73549022e84
Marc B <hellhand@gmail.com>
2011-03-17 00:41:21 +0100

    Minor bugfix

--- gen/com/hh/R.java
+++ gen/com/hh/R.java
@@ -12,7 +12,6 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_settings_sound=0x7f020000;
-        public static final int icon=0x7f020001;
     }
     public static final class id {
         public static final int alarm_volume_seekbar=0x7f050002;
